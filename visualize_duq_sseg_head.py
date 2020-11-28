1417f41afa3f17b783263d025e74e4d47c76276a
yimengli46 <yli44@ARGO-1.orc.gmu.edu>
2020-11-28 15:22:47 -0500

    add batch processing in visualization code

--- visualize_duq_sseg_head.py
+++ visualize_duq_sseg_head.py
@@ -18,109 +18,111 @@ dataset = 'fishyscapes' #'lostAndFound', 'cityscapes', 'fishyscapes', 'roadAnoma
 rep_style = 'both' #'both', 'ObjDet', 'SSeg' 
 save_option = 'both' #'image', 'npy', 'both'
 base_folder = 'visualization/whole'
-ignore_background_uncertainty = False
+ignore_background_uncertainty = True
 
-saved_folder = '{}/obj_sseg_{}/{}'.format(base_folder, style, dataset)
-trained_model_dir = 'trained_model/whole/{}'.format(style)
+for dataset in ['lostAndFound', 'roadAnomaly', 'fishyscapes']:
 
-# check if folder exists
-if not os.path.exists('{}/obj_sseg_{}'.format(base_folder, style)):
-	os.mkdir('{}/obj_sseg_{}'.format(base_folder, style))
-if not os.path.exists(saved_folder): 
-    os.mkdir(saved_folder)
+	saved_folder = '{}/obj_sseg_{}/{}'.format(base_folder, style, dataset)
+	trained_model_dir = 'trained_model/whole/{}'.format(style)
 
-if dataset == 'cityscapes':
-	dataset_folder = '/projects/kosecka/yimeng/Datasets/Cityscapes'
-	ds_val = CityscapesProposalsDataset(dataset_folder, 'val', rep_style=rep_style)
-elif dataset == 'lostAndFound':
-	dataset_folder = '/projects/kosecka/yimeng/Datasets/Lost_and_Found'
-	ds_val = LostAndFoundProposalsDataset(dataset_folder, rep_style=rep_style)
-elif dataset == 'fishyscapes':
-	dataset_folder = '/projects/kosecka/yimeng/Datasets/Fishyscapes_Static'
-	ds_val = FishyscapesProposalsDataset(dataset_folder, rep_style=rep_style)
-elif dataset == 'roadAnomaly':
-	dataset_folder = '/projects/kosecka/yimeng/Datasets/RoadAnomaly'
-	ds_val = RoadAnomalyProposalsDataset(dataset_folder, rep_style=rep_style)
-num_classes = ds_val.NUM_CLASSES
+	# check if folder exists
+	if not os.path.exists('{}/obj_sseg_{}'.format(base_folder, style)):
+		os.mkdir('{}/obj_sseg_{}'.format(base_folder, style))
+	if not os.path.exists(saved_folder): 
+	    os.mkdir(saved_folder)
 
-if rep_style == 'both':
-    input_dim = 512
-else:
-    input_dim = 256
+	if dataset == 'cityscapes':
+		dataset_folder = '/projects/kosecka/yimeng/Datasets/Cityscapes'
+		ds_val = CityscapesProposalsDataset(dataset_folder, 'val', rep_style=rep_style)
+	elif dataset == 'lostAndFound':
+		dataset_folder = '/projects/kosecka/yimeng/Datasets/Lost_and_Found'
+		ds_val = LostAndFoundProposalsDataset(dataset_folder, rep_style=rep_style)
+	elif dataset == 'fishyscapes':
+		dataset_folder = '/projects/kosecka/yimeng/Datasets/Fishyscapes_Static'
+		ds_val = FishyscapesProposalsDataset(dataset_folder, rep_style=rep_style)
+	elif dataset == 'roadAnomaly':
+		dataset_folder = '/projects/kosecka/yimeng/Datasets/RoadAnomaly'
+		ds_val = RoadAnomalyProposalsDataset(dataset_folder, rep_style=rep_style)
+	num_classes = ds_val.NUM_CLASSES
 
-device = torch.device('cuda')
+	if rep_style == 'both':
+	    input_dim = 512
+	else:
+	    input_dim = 256
 
-classifier = DuqHead(num_classes, input_dim).to(device)
-classifier.load_state_dict(torch.load('{}/{}_classifier_0.0.pth'.format(trained_model_dir, style)))
-#assert 1==2
+	device = torch.device('cuda')
 
-if dataset == 'cityscapes':
-	num_vis = 50
-elif dataset == 'lostAndFound':
-	num_vis = len(ds_val)
-elif dataset == 'fishyscapes':
-	num_vis = len(ds_val)
-elif dataset == 'roadAnomaly':
-	num_vis = len(ds_val)
+	classifier = DuqHead(num_classes, input_dim).to(device)
+	classifier.load_state_dict(torch.load('{}/{}_classifier_0.0.pth'.format(trained_model_dir, style)))
+	#assert 1==2
 
-with torch.no_grad():
-	for i in range(num_vis):
-		
-		print('i = {}'.format(i))
-		whole_feature, rgb_img, sseg_label = ds_val.get_whole_img(i)
+	if dataset == 'cityscapes':
+		num_vis = 50
+	elif dataset == 'lostAndFound':
+		num_vis = len(ds_val)
+	elif dataset == 'fishyscapes':
+		num_vis = len(ds_val)
+	elif dataset == 'roadAnomaly':
+		num_vis = len(ds_val)
 
-		whole_feature = whole_feature.to(device)
-		logits = classifier(whole_feature)
+	with torch.no_grad():
+		for i in range(num_vis):
+			
+			print('i = {}'.format(i))
+			whole_feature, rgb_img, sseg_label = ds_val.get_whole_img(i)
 
-		sseg_pred = torch.argmax(logits, dim=1)
+			whole_feature = whole_feature.to(device)
+			logits = classifier(whole_feature)
 
-		logits = logits.cpu().numpy()[0]
-		sseg_pred = sseg_pred.cpu().numpy()[0]
+			sseg_pred = torch.argmax(logits, dim=1)
 
-		uncertainty = 1.0 - np.amax(logits, axis=0)
+			logits = logits.cpu().numpy()[0]
+			sseg_pred = sseg_pred.cpu().numpy()[0]
 
-		if ignore_background_uncertainty:
-			# ignore uncertainty on the background pixels
-			uncertainty[sseg_pred == 0] = 0 # road
-			uncertainty[sseg_pred == 1] = 0 # building
-			uncertainty[sseg_pred == 3] = 0 # vegetation
-			uncertainty[sseg_pred == 4] = 0 # sky
+			uncertainty = 1.0 - np.amax(logits, axis=0)
 
-		if dataset == 'cityscapes':
-			color_sseg_label = apply_color_map(sseg_label)
-		else:
-			color_sseg_label = sseg_label
-		color_sseg_pred = apply_color_map(sseg_pred)
-		#assert 1==2
+			if ignore_background_uncertainty:
+				# ignore uncertainty on the background pixels
+				uncertainty[sseg_pred == 0] = 0 # road
+				uncertainty[sseg_pred == 1] = 0 # building
+				uncertainty[sseg_pred == 3] = 0 # vegetation
+				uncertainty[sseg_pred == 4] = 0 # sky
 
-		if save_option == 'both' or save_option == 'image':
-			fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(18,10))
-			ax[0][0].imshow(rgb_img)
-			ax[0][0].get_xaxis().set_visible(False)
-			ax[0][0].get_yaxis().set_visible(False)
-			ax[0][0].set_title("rgb image")
-			ax[0][1].imshow(color_sseg_label)
-			ax[0][1].get_xaxis().set_visible(False)
-			ax[0][1].get_yaxis().set_visible(False)
-			ax[0][1].set_title("sseg label")
-			ax[1][0].imshow(color_sseg_pred)
-			ax[1][0].get_xaxis().set_visible(False)
-			ax[1][0].get_yaxis().set_visible(False)
-			ax[1][0].set_title("sseg pred")
-			ax[1][1].imshow(uncertainty, vmin=0.0, vmax=1.0)
-			ax[1][1].get_xaxis().set_visible(False)
-			ax[1][1].get_yaxis().set_visible(False)
-			ax[1][1].set_title("uncertainty")
+			if dataset == 'cityscapes':
+				color_sseg_label = apply_color_map(sseg_label)
+			else:
+				color_sseg_label = sseg_label
+			color_sseg_pred = apply_color_map(sseg_pred)
+			#assert 1==2
 
-			fig.tight_layout()
-			fig.savefig('{}/img_{}.jpg'.format(saved_folder, i))
-			plt.close()
+			if save_option == 'both' or save_option == 'image':
+				fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(18,10))
+				ax[0][0].imshow(rgb_img)
+				ax[0][0].get_xaxis().set_visible(False)
+				ax[0][0].get_yaxis().set_visible(False)
+				ax[0][0].set_title("rgb image")
+				ax[0][1].imshow(color_sseg_label)
+				ax[0][1].get_xaxis().set_visible(False)
+				ax[0][1].get_yaxis().set_visible(False)
+				ax[0][1].set_title("sseg label")
+				ax[1][0].imshow(color_sseg_pred)
+				ax[1][0].get_xaxis().set_visible(False)
+				ax[1][0].get_yaxis().set_visible(False)
+				ax[1][0].set_title("sseg pred")
+				ax[1][1].imshow(uncertainty, vmin=0.0, vmax=1.0)
+				ax[1][1].get_xaxis().set_visible(False)
+				ax[1][1].get_yaxis().set_visible(False)
+				ax[1][1].set_title("uncertainty")
 
-		if save_option == 'both' or save_option == 'npy':
-			result = {}
-			result['sseg'] = sseg_pred
-			result['uncertainty'] = uncertainty
-			np.save('{}/img_{}.npy'.format(saved_folder, i), result)
-		
-		#assert 1==2
+				fig.tight_layout()
+				fig.savefig('{}/img_{}.jpg'.format(saved_folder, i))
+				plt.close()
+
+			if save_option == 'both' or save_option == 'npy':
+				result = {}
+				result['sseg'] = sseg_pred
+				result['uncertainty'] = uncertainty
+				np.save('{}/img_{}.npy'.format(saved_folder, i), result)
+			
+			#assert 1==2
 
