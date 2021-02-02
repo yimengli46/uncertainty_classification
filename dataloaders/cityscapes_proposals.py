095d4a281bfae52f96c03462d8ae2672542f68da
yimengli46 <yli44@ARGO-2.orc.gmu.edu>
2021-02-02 17:54:04 -0500

    extract feature through pooler

--- dataloaders/cityscapes_proposals.py
+++ dataloaders/cityscapes_proposals.py
@@ -9,6 +9,7 @@ import torch.utils.data as data
 import torch
 import torch.nn.functional as F
 from torchvision.ops import roi_align
+from .pooler import ROIPooler
 
 device = torch.device('cuda')
 
@@ -34,79 +35,98 @@ class CityscapesProposalsDataset(data.Dataset):
 
 		print("Found {} {} images".format(len(self.img_list), self.split))
 
+		self.pooler = ROIPooler(
+			output_size=14, 
+			scales=[1.0/4, 1.0/8, 1.0/16, 1.0/32],
+			sampling_ratio=0)
+
 		# proposal, mask feature and sseg feature folder
-		self.proposal_folder = '/scratch/yli44/detectron2/my_projects/Bayesian_MaskRCNN/generated_proposals_cityscapes_ignore_person/cityscapes_{}'.format(self.mode)
-		self.mask_ft_folder  = '/scratch/yli44/detectron2/my_projects/Bayesian_MaskRCNN/proposal_mask_features_cityscapes_ignore_person/cityscapes_{}'.format(self.mode)
+		if self.mode == 'train':
+			self.proposal_folder = '/scratch/yli44/detectron2/my_projects/Bayesian_MaskRCNN/generated_proposals_cityscapes_ignore_person/cityscapes_{}'.format(self.mode)
+			self.mask_ft_folder  = '/scratch/yli44/detectron2/my_projects/Bayesian_MaskRCNN/proposal_mask_features_cityscapes_ignore_person/cityscapes_{}'.format(self.mode)
+		elif self.mode == 'val':
+			self.proposal_folder = '/scratch/yli44/detectron2/my_projects/Bayesian_MaskRCNN/generated_proposals_whole/cityscapes_{}'.format(self.mode)
+			self.mask_ft_folder  = '/scratch/yli44/detectron2/my_projects/Bayesian_MaskRCNN/proposal_mask_features_cityscapes_ignore_person/cityscapes_{}'.format(self.mode)
 		self.sseg_ft_folder  = '/projects/kosecka/yimeng/Datasets/Cityscapes/deeplab_ft_8_classes/{}'.format(self.mode)
 
 	def __len__(self):
 		return len(self.img_list)
 
+	def read_proposals(self, proposal_results, img_H, img_W):
+		H = proposal_results['height']
+		W = proposal_results['width']
+		proposals = proposal_results['proposal']
+
+		scale_x, scale_y = (img_W / W, img_H / H)
+
+		feature_proposals = proposals.copy().astype(np.float32)
+		img_proposals = proposals.copy().astype(np.float32)
+		#print('img_proposals.dtype = {}'.format(img_proposals.dtype))
+
+		img_proposals[:, 0::2] *= scale_x
+		img_proposals[:, 1::2] *= scale_y
+
+		np.clip(img_proposals[:, 0], 0, img_W, out=img_proposals[:, 0])
+		np.clip(img_proposals[:, 1], 0, img_H, out=img_proposals[:, 1])
+		np.clip(img_proposals[:, 2], 0, img_W, out=img_proposals[:, 2])
+		np.clip(img_proposals[:, 3], 0, img_H, out=img_proposals[:, 3])
+
+		# check if proposal size is large enough
+		w_diff, h_diff = (img_proposals[:, 2] - img_proposals[:, 0], img_proposals[:, 3] - img_proposals[:, 1])
+		mask = (w_diff >= 2) & (h_diff >= 2) # x2 - x1 should be at least 2.
+
+		img_proposals = img_proposals[mask]
+		feature_proposals = feature_proposals[mask] 
+
+		return img_proposals, feature_proposals
+
 	def __getitem__(self, i):
 		img_path = '{}/{}'.format(self.dataset_dir, self.img_list[i]['left_img'])
 		lbl_path = '{}/{}'.format(self.dataset_dir, self.img_list[i]['semSeg'])
-		#print('img_path = {}'.format(img_path))
 
 		rgb_img = np.array(Image.open(img_path).convert('RGB'))
+		H, W, _ = rgb_img.shape
 		sseg_label = np.array(Image.open(lbl_path), dtype=np.uint8)
 		sseg_label = self.encode_segmap(sseg_label) # 1024 x 2048
 		#print('sseg_label.shape = {}'.format(sseg_label.shape))
 		
 		# read proposals
-		proposals = np.load('{}/{}_proposal.npy'.format(self.proposal_folder, i), allow_pickle=True)
+		proposal_results = np.load('{}/{}_proposal.npy'.format(self.proposal_folder, i), allow_pickle=True).item()
+		img_proposals, feature_proposals = self.read_proposals(proposal_results, H, W)
+
 		# read mask features
-		mask_feature = np.load('{}/{}_proposal_mask_features.npy'.format(self.mask_ft_folder, i), allow_pickle=True)
-		#print('mask_feature.shape = {}'.format(mask_feature.shape))
+		whole_feature = np.load('{}/{}_whole_features.npy'.format(self.mask_ft_folder, i), allow_pickle=True).item()
+		# 'p6' is not used 
+		obj_feature = [torch.tensor(whole_feature[k]).to(device) for k in ['p2', 'p3', 'p4', 'p5']]
+
+		#print('obj_feature.shape = {}'.format(obj_feature.shape))
 		# read sseg features
 		sseg_feature = np.load('{}/{}_deeplab_ft.npy'.format(self.sseg_ft_folder, i), allow_pickle=True) # 256 x 128 x 256
-		_, H, W = sseg_feature.shape
 		#print('sseg_feature.shape = {}'.format(sseg_feature.shape))
-
 		sseg_feature = torch.tensor(sseg_feature).unsqueeze(0).to(device) # 1 x 256 x 128 x 256
 
-		index = np.random.choice(100, self.batch_size, replace=False)
-		#index = np.array([0,1,2])
-		proposals = proposals[index] # B x 4
-		mask_feature = torch.tensor(mask_feature[index]).to(device) # B x 256 x 14 x 14
+		N, _ = img_proposals.shape
+		#if self.split == 'train':
+		#	N = 100 # training stage only pick from the top 100
+		index = np.random.choice(N, self.batch_size, replace=False)
+		img_proposals = img_proposals[index] # B x 4
+		feature_proposals = feature_proposals[index]
 
-		#print('proposals.shape = {}'.format(proposals.shape))
-		#print('mask_feature.shape = {}'.format(mask_feature.shape))
-
-		#batch_sseg_feature = torch.zeros((self.batch_size, 256, 14, 14))
 		batch_sseg_label = torch.zeros((self.batch_size, 28, 28))
-		batch_prop_boxes = torch.zeros((self.batch_size, 4)) 
+		batch_prop_boxes = torch.tensor(img_proposals).to(device)
+		batch_feature_prop_boxes = torch.tensor(feature_proposals).to(device)
 
 		for j in range(self.batch_size):
-			x1, y1, x2, y2 = proposals[j]
+			x1, y1, x2, y2 = img_proposals[j]
 
-			# in case the proposal is very small
-			x1, y1, x2, y2 = round(x1), round(y1), round(x2), round(y2)
-			if x2 - x1 < 2:
-				x2 = x1 + 2
-			if y2 - y1 < 2:
-				y2 = y1 + 2
-
-			prop_x1 = int(max(round(x1), 0))
-			prop_y1 = int(max(round(y1), 0))
-			prop_x2 = int(min(round(x2), 2048))
-			prop_y2 = int(min(round(y2), 1024))
-
-			# in case the proposal is outside
-			if prop_x2 == 2048:
-				prop_x1 = min(prop_x1, prop_x2 - 2)
-			if prop_y2 == 1024:
-				prop_y1 = min(prop_y1, prop_y2 - 2)
-
-			#print('prop_x1 = {}, prop_y1 = {}, prop_x2 = {}, prop_y2 = {}'.format(prop_x1, prop_y1, prop_x2, prop_y2))
+			prop_x1 = int(round(x1))
+			prop_y1 = int(round(y1))
+			prop_x2 = int(round(x2))
+			prop_y2 = int(round(y2))
 
 			img_patch = rgb_img[prop_y1:prop_y2, prop_x1:prop_x2]
 			sseg_label_patch = sseg_label[prop_y1:prop_y2, prop_x1:prop_x2]
 
-			batch_prop_boxes[j, 0] = prop_x1
-			batch_prop_boxes[j, 1] = prop_y1
-			batch_prop_boxes[j, 2] = prop_x2
-			batch_prop_boxes[j, 3] = prop_y2
-			
 			'''
 			# visualize for test
 			fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10,5))
@@ -126,14 +146,17 @@ class CityscapesProposalsDataset(data.Dataset):
 			#print('sseg_label_patch.shape = {}'.format(sseg_label_patch.shape))
 			batch_sseg_label[j] = torch.tensor(sseg_label_patch)
 
-		batch_prop_boxes = batch_prop_boxes.to(device)
-		batch_sseg_feature = roi_align(sseg_feature, [batch_prop_boxes], output_size=(14, 14), spatial_scale=1/8.0)
+		#print('batch_prop_boxes = {}'.format(batch_prop_boxes))
+		batch_sseg_feature = roi_align(sseg_feature, [batch_prop_boxes], output_size=(14, 14), spatial_scale=1/8.0, aligned=True)
+		batch_obj_feature  = self.pooler(obj_feature, [batch_feature_prop_boxes])
+		#print('batch_obj_feature.shape = {}'.format(batch_obj_feature.shape))
+		#print('batch_obj_feature = {}'.format(batch_obj_feature))
 		#print('batch_sseg_feature.shape = {}'.format(batch_sseg_feature.shape))
 
 		if self.rep_style == 'both':
-			patch_feature = torch.cat((mask_feature, batch_sseg_feature), dim=1) # B x 512 x 14 x 14
+			patch_feature = torch.cat((batch_obj_feature, batch_sseg_feature), dim=1) # B x 512 x 14 x 14
 		elif self.rep_style == 'ObjDet':
-			patch_feature = mask_feature
+			patch_feature = batch_obj_feature
 		elif self.rep_style == 'SSeg':
 			patch_feature = batch_sseg_feature 
 		#print('patch_feature.shape = {}'.format(patch_feature.shape))
@@ -168,77 +191,87 @@ class CityscapesProposalsDataset(data.Dataset):
 		return mask
 
 	def get_proposal(self, i, j=0):
-		img_path = '{}/{}'.format(self.dataset_dir, self.img_list[i]['rgb_path'])
-		lbl_path = '{}/{}'.format(self.dataset_dir, self.img_list[i]['semSeg_path'])
+		img_path = '{}/{}'.format(self.dataset_dir, self.img_list[i]['left_img'])
+		lbl_path = '{}/{}'.format(self.dataset_dir, self.img_list[i]['semSeg'])
 
 		rgb_img = np.array(Image.open(img_path).convert('RGB'))
+		H, W, _ = rgb_img.shape
 		sseg_label = np.array(Image.open(lbl_path), dtype=np.uint8)
 		sseg_label = self.encode_segmap(sseg_label) # 1024 x 2048
-		print('sseg_label.shape = {}'.format(sseg_label.shape))
+		#print('sseg_label.shape = {}'.format(sseg_label.shape))
 		
 		# read proposals
-		proposals = np.load('{}/{}_proposal.npy'.format(self.proposal_folder, i), allow_pickle=True)
+		proposal_results = np.load('{}/{}_proposal.npy'.format(self.proposal_folder, i), allow_pickle=True).item()
+		img_proposals, feature_proposals = self.read_proposals(proposal_results, H, W)
+
 		# read mask features
-		mask_feature = np.load('{}/{}_proposal_mask_features.npy'.format(self.mask_ft_folder, i), allow_pickle=True)
+		whole_feature = np.load('{}/{}_whole_features.npy'.format(self.mask_ft_folder, i), allow_pickle=True).item()
+		# 'p6' is not used 
+		obj_feature = [torch.tensor(whole_feature[k]).to(device) for k in ['p2', 'p3', 'p4', 'p5']]
+
+		#print('obj_feature.shape = {}'.format(obj_feature.shape))
 		# read sseg features
 		sseg_feature = np.load('{}/{}_deeplab_ft.npy'.format(self.sseg_ft_folder, i), allow_pickle=True) # 256 x 128 x 256
-		_, H, W = sseg_feature.shape
 		#print('sseg_feature.shape = {}'.format(sseg_feature.shape))
-
 		sseg_feature = torch.tensor(sseg_feature).unsqueeze(0).to(device) # 1 x 256 x 128 x 256
-		
-		index = np.array([j])
-		proposals = proposals[index] # B x 4
-		mask_feature = torch.tensor(mask_feature[index]).to(device) # B x 256 x 14 x 14
 
-		batch_sseg_label = torch.zeros((1, 28, 28))
-		batch_prop_boxes = torch.zeros((1, 4))
-		
-		x1, y1, x2, y2 = proposals[0]
-		prop_x1 = int(max(round(x1), 0))
-		prop_y1 = int(max(round(y1), 0))
-		prop_x2 = int(min(round(x2), 2048-1))
-		prop_y2 = int(min(round(y2), 1024-1))
+		N, _ = img_proposals.shape
+		#index = np.random.choice(N, self.batch_size, replace=False)
+		img_proposals = np.expand_dims(img_proposals[j], axis=0) # B x 4
+		feature_proposals = np.expand_dims(feature_proposals[j], axis=0)
+		#print('img_proposals.shape = {}'.format(img_proposals.shape))
+		#print('feature_proposals.shape = {}'.format(feature_proposals.shape))
+
+		batch_sseg_label = torch.zeros((self.batch_size, 28, 28))
+		batch_prop_boxes = torch.tensor(img_proposals).to(device)
+		batch_feature_prop_boxes = torch.tensor(feature_proposals).to(device)
+
+		x1, y1, x2, y2 = img_proposals[0]
+
+		prop_x1 = int(round(x1))
+		prop_y1 = int(round(y1))
+		prop_x2 = int(round(x2))
+		prop_y2 = int(round(y2))
 
 		img_proposal = rgb_img[prop_y1:prop_y2, prop_x1:prop_x2]
 		sseg_label_proposal = sseg_label[prop_y1:prop_y2, prop_x1:prop_x2]
 
-		batch_prop_boxes[0, 0] = prop_x1
-		batch_prop_boxes[0, 1] = prop_y1
-		batch_prop_boxes[0, 2] = prop_x2
-		batch_prop_boxes[0, 3] = prop_y2
-		
 		'''
 		# visualize for test
 		fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10,5))
-		ax[0].imshow(img_proposal)
+		ax[0].imshow(img_patch)
 		ax[0].get_xaxis().set_visible(False)
 		ax[0].get_yaxis().set_visible(False)
-		ax[0].set_title("rgb proposal")
-		ax[1].imshow(sseg_label_proposal, vmin=0.0, vmax=8.0)
+		ax[0].set_title("rgb image")
+		ax[1].imshow(sseg_label_patch, vmin=0.0, vmax=8.0)
 		ax[1].get_xaxis().set_visible(False)
 		ax[1].get_yaxis().set_visible(False)
-		ax[1].set_title("sseg label proposal")
+		ax[1].set_title("sseg label")
 		plt.show()
 		'''
 
 		# rescale sseg label to 28x28
 		sseg_label_patch = cv2.resize(sseg_label_proposal, (28, 28), interpolation=cv2.INTER_NEAREST) # 28 x 28
-		sseg_label_patch = sseg_label_patch.astype('int')
-		#print('sseg_label_patch = {}'.format(sseg_label_patch))
+		#print('sseg_label_patch.shape = {}'.format(sseg_label_patch.shape))
 		batch_sseg_label[0] = torch.tensor(sseg_label_patch)
 
-		batch_prop_boxes = batch_prop_boxes.to(device)
-		batch_sseg_feature = roi_align(sseg_feature, [batch_prop_boxes], output_size=(14, 14), spatial_scale=1/8.0)
+		#print('batch_prop_boxes = {}'.format(batch_prop_boxes))
+		batch_sseg_feature = roi_align(sseg_feature, [batch_prop_boxes], output_size=(14, 14), spatial_scale=1/8.0, aligned=True)
+		batch_obj_feature  = self.pooler(obj_feature, [batch_feature_prop_boxes])
+		#print('batch_obj_feature.shape = {}'.format(batch_obj_feature.shape))
+		#print('batch_obj_feature = {}'.format(batch_obj_feature))
+		#print('batch_sseg_feature.shape = {}'.format(batch_sseg_feature.shape))
 
 		if self.rep_style == 'both':
-			patch_feature = torch.cat((mask_feature, batch_sseg_feature), dim=1) # B x 512 x 14 x 14
+			patch_feature = torch.cat((batch_obj_feature, batch_sseg_feature), dim=1) # B x 512 x 14 x 14
 		elif self.rep_style == 'ObjDet':
-			patch_feature = mask_feature
+			patch_feature = batch_obj_feature
 		elif self.rep_style == 'SSeg':
-			patch_feature = batch_sseg_feature
+			patch_feature = batch_sseg_feature 
+		#print('patch_feature.shape = {}'.format(patch_feature.shape))
 
 		return patch_feature, batch_sseg_label, img_proposal, sseg_label_proposal
+		#assert 1==2
 
 '''
 cityscapes_train = CityscapesProposalsDataset('/projects/kosecka/yimeng/Datasets/Cityscapes', 'train', batch_size=32, rep_style='both')
---
+++ dataloaders/pooler.py
@@ -0,0 +1,218 @@
+import math
+import sys
+import torch
+from torch import nn
+from torchvision.ops import roi_align
+
+def cat(tensors, dim=0):
+    """
+    Efficient version of torch.cat that avoids a copy if there is only a single element in a list
+    """
+    assert isinstance(tensors, (list, tuple))
+    if len(tensors) == 1:
+        return tensors[0]
+    return torch.cat(tensors, dim)
+
+class ROIAlign(nn.Module):
+    def __init__(self, output_size, spatial_scale, sampling_ratio, aligned=True):
+        """
+        Args:
+            output_size (tuple): h, w
+            spatial_scale (float): scale the input boxes by this number
+            sampling_ratio (int): number of inputs samples to take for each output
+                sample. 0 to take samples densely.
+            aligned (bool): if False, use the legacy implementation in
+                Detectron. If True, align the results more perfectly.
+
+        Note:
+            The meaning of aligned=True:
+
+            Given a continuous coordinate c, its two neighboring pixel indices (in our
+            pixel model) are computed by floor(c - 0.5) and ceil(c - 0.5). For example,
+            c=1.3 has pixel neighbors with discrete indices [0] and [1] (which are sampled
+            from the underlying signal at continuous coordinates 0.5 and 1.5). But the original
+            roi_align (aligned=False) does not subtract the 0.5 when computing neighboring
+            pixel indices and therefore it uses pixels with a slightly incorrect alignment
+            (relative to our pixel model) when performing bilinear interpolation.
+
+            With `aligned=True`,
+            we first appropriately scale the ROI and then shift it by -0.5
+            prior to calling roi_align. This produces the correct neighbors; see
+            detectron2/tests/test_roi_align.py for verification.
+
+            The difference does not make a difference to the model's performance if
+            ROIAlign is used together with conv layers.
+        """
+        super(ROIAlign, self).__init__()
+        self.output_size = output_size
+        self.spatial_scale = spatial_scale
+        self.sampling_ratio = sampling_ratio
+        self.aligned = aligned
+
+    def forward(self, input, rois):
+        """
+        Args:
+            input: NCHW images
+            rois: [Bx4] boxes. First column is the index into N. The other 4 columns are xyxy.
+        """
+        #assert rois.dim() == 2 and rois.size(1) == 5
+        return roi_align(
+            input, rois, self.output_size, self.spatial_scale, self.sampling_ratio, self.aligned
+        )
+
+def area(box):
+	area = (box[:, 2] - box[:, 0]) * (box[:, 3] - box[:, 1])
+	return area
+
+def assign_boxes_to_levels(box_lists, min_level, max_level, canonical_box_size, canonical_level):
+    """
+    Map each box in `box_lists` to a feature map level index and return the assignment
+    vector.
+
+    Args:
+        box_lists (list[Boxes] | list[RotatedBoxes]): A list of N Boxes or N RotatedBoxes,
+            where N is the number of images in the batch.
+        min_level (int): Smallest feature map level index. The input is considered index 0,
+            the output of stage 1 is index 1, and so.
+        max_level (int): Largest feature map level index.
+        canonical_box_size (int): A canonical box size in pixels (sqrt(box area)).
+        canonical_level (int): The feature map level index on which a canonically-sized box
+            should be placed.
+
+    Returns:
+        A tensor of length M, where M is the total number of boxes aggregated over all
+            N batch images. The memory layout corresponds to the concatenation of boxes
+            from all images. Each element is the feature map index, as an offset from
+            `self.min_level`, for the corresponding box (so value i means the box is at
+            `self.min_level + i`).
+    """
+    eps = sys.float_info.epsilon
+    box_sizes = torch.sqrt(cat([area(boxes) for boxes in box_lists]))
+    # Eqn.(1) in FPN paper
+    level_assignments = torch.floor(
+        canonical_level + torch.log2(box_sizes / canonical_box_size + eps)
+    )
+    # clamp level to (min, max), in case the box size is too large or too small
+    # for the available feature maps
+    level_assignments = torch.clamp(level_assignments, min=min_level, max=max_level)
+    return level_assignments.to(torch.int64) - min_level
+
+
+
+class ROIPooler(nn.Module):
+    """
+    Region of interest feature map pooler that supports pooling from one or more
+    feature maps.
+    """
+
+    def __init__(
+        self,
+        output_size,
+        scales,
+        sampling_ratio,
+        canonical_box_size=224,
+        canonical_level=4,
+    ):
+        """
+        Args:
+            output_size (int, tuple[int] or list[int]): output size of the pooled region,
+                e.g., 14 x 14. If tuple or list is given, the length must be 2.
+            scales (list[float]): The scale for each low-level pooling op relative to
+                the input image. For a feature map with stride s relative to the input
+                image, scale is defined as a 1 / s. The stride must be power of 2.
+                When there are multiple scales, they must form a pyramid, i.e. they must be
+                a monotically decreasing geometric sequence with a factor of 1/2.
+            sampling_ratio (int): The `sampling_ratio` parameter for the ROIAlign op.
+            canonical_box_size (int): A canonical box size in pixels (sqrt(box area)). The default
+                is heuristically defined as 224 pixels in the FPN paper (based on ImageNet
+                pre-training).
+            canonical_level (int): The feature map level index from which a canonically-sized box
+                should be placed. The default is defined as level 4 (stride=16) in the FPN paper,
+                i.e., a box of size 224x224 will be placed on the feature with stride=16.
+                The box placement for all boxes will be determined from their sizes w.r.t
+                canonical_box_size. For example, a box whose area is 4x that of a canonical box
+                should be used to pool features from feature level ``canonical_level+1``.
+
+                Note that the actual input feature maps given to this module may not have
+                sufficiently many levels for the input boxes. If the boxes are too large or too
+                small for the input feature maps, the closest level will be used.
+        """
+        super().__init__()
+
+        if isinstance(output_size, int):
+            output_size = (output_size, output_size)
+        assert len(output_size) == 2
+        assert isinstance(output_size[0], int) and isinstance(output_size[1], int)
+        self.output_size = output_size
+        
+        self.level_poolers = nn.ModuleList(
+            ROIAlign(
+                output_size, spatial_scale=scale, sampling_ratio=sampling_ratio, aligned=True
+            )
+            for scale in scales
+        )
+
+        # Map scale (defined as 1 / stride) to its feature map level under the
+        # assumption that stride is a power of 2.
+        min_level = -math.log2(scales[0])
+        max_level = -math.log2(scales[-1])
+        assert math.isclose(min_level, int(min_level)) and math.isclose(
+            max_level, int(max_level)
+        ), "Featuremap stride is not power of 2!"
+        self.min_level = int(min_level)
+        self.max_level = int(max_level)
+        assert (
+            len(scales) == self.max_level - self.min_level + 1
+        ), "[ROIPooler] Sizes of input featuremaps do not form a pyramid!"
+        assert 0 < self.min_level and self.min_level <= self.max_level
+        if len(scales) > 1:
+            # When there is only one feature map, canonical_level is redundant and we should not
+            # require it to be a sensible value. Therefore we skip this assertion
+            assert self.min_level <= canonical_level and canonical_level <= self.max_level
+        self.canonical_level = canonical_level
+        assert canonical_box_size > 0
+        self.canonical_box_size = canonical_box_size
+
+    def forward(self, x, box_lists):
+        """
+        Args:
+            x (list[Tensor]): A list of feature maps of NCHW shape, with scales matching those
+                used to construct this module.
+            box_lists (list[Boxes] | list[RotatedBoxes]):
+                A list of N Boxes or N RotatedBoxes, where N is the number of images in the batch.
+                The box coordinates are defined on the original image and
+                will be scaled by the `scales` argument of :class:`ROIPooler`.
+
+        Returns:
+            Tensor:
+                A tensor of shape (M, C, output_size, output_size) where M is the total number of
+                boxes aggregated over all N batch images and C is the number of channels in `x`.
+        """
+        num_level_assignments = len(self.level_poolers)
+
+        pooler_fmt_boxes = box_lists[0]
+
+        if num_level_assignments == 1:
+            return self.level_poolers[0](x[0], pooler_fmt_boxes)
+
+        level_assignments = assign_boxes_to_levels(
+            box_lists, self.min_level, self.max_level, self.canonical_box_size, self.canonical_level
+        )
+
+        num_boxes = pooler_fmt_boxes.shape[0]
+        num_channels = x[0].shape[1]
+        output_size = self.output_size[0]
+
+        dtype, device = x[0].dtype, x[0].device
+        output = torch.zeros(
+            (num_boxes, num_channels, output_size, output_size), dtype=dtype, device=device
+        )
+
+        for level, (x_level, pooler) in enumerate(zip(x, self.level_poolers)):
+            inds = torch.nonzero(level_assignments == level).squeeze(1)
+            #print('inds = {}'.format(inds))
+            pooler_fmt_boxes_level = pooler_fmt_boxes[inds]
+            #print('boxes_level = {}'.format(pooler_fmt_boxes_level))
+            output[inds] = pooler(x_level, [pooler_fmt_boxes_level])
+
+        return output
